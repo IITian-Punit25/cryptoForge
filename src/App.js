04aec408e8b2fd285fedd220609a7d1ad7e5fc88
IITian-Punit25 <punitkashyap2101@gmail.com>
2025-07-08 23:51:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Encrypt from './fontend/Encrypt,js';
 import Navbar from './fontend/Navbar';
 import Decrypt from "./fontend/Decrypt";
 import About from "./fontend/About";
 
 function App() {
-  return (
-    <div>
-        <Navbar/>
-        <Routes>
-            <Route path="/" element={<Encrypt/>}/>
-            <Route path="/decrypt" element={<Decrypt/>}/>
-            <Route path="/about" element={<About/>}/>
-        </Routes>
-    </div>
-  );
+    return (
+        <BrowserRouter basename="/cryptoForge">
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Encrypt />} />
+                <Route path="/decrypt" element={<Decrypt />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
